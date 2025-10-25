c663956f4b8d377d4bb940a631df1cd29b5407c3
Arsln-mahmt <arslnmahmt198@gmail.com>
2025-10-25 23:00:11 +0300

    Initial Commit

---
+++ ReservationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReservationApp
+//
+//  Created by Mahmut Arslan on 25.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReservationApp/ReservationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationAppApp.swift
+//  ReservationApp
+//
+//  Created by Mahmut Arslan on 25.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ReservationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
